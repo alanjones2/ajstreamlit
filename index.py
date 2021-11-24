1cfbb88ef2ddcaf610f8968001dd72f8f4ae1583
Alan Jones <jones.alan@hotmail.co.uk>
2021-11-24 12:12:57 +0100

    remove finance

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout = "wide")
 
-page = st.sidebar.selectbox('Select app',['Global stats','Rational UI Design','Finance']) 
+page = st.sidebar.selectbox('Select app',['Global stats','Rational UI Design'])#,'Finance']) 
 if page == 'Global stats':
 	from stlib import sttest4
 	sttest4.run()
